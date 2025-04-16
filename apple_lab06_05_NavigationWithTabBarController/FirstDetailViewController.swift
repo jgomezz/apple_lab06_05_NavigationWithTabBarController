106bbf0de405502200967c6d813ea43a0e4fba97
Jaime <jgomezm@tecsup.edu.pe>
2025-04-16 17:50:33 -0500

    first commit

---
+++ apple_lab06_05_NavigationWithTabBarController/FirstDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FirstDetailViewController.swift
+//  apple_lab06_05_NavigationWithTabBarController
+//
+//  Created by developer on 4/16/25.
+//
+
+import UIKit
+
+class FirstDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
