e32d92342c126d762bc7983d930f4ba09e745777
Jaime <jgomezm@tecsup.edu.pe>
2025-04-16 16:01:25 -0500

    Initial Commit

---
+++ apple_lab06_05_NavigationWithTabBarController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  apple_lab06_05_NavigationWithTabBarController
+//
+//  Created by developer on 4/16/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ apple_lab06_05_NavigationWithTabBarControllerTests/apple_lab06_05_NavigationWithTabBarControllerTests.swift
@@ -0,0 +1,17 @@
+//
+//  apple_lab06_05_NavigationWithTabBarControllerTests.swift
+//  apple_lab06_05_NavigationWithTabBarControllerTests
+//
+//  Created by developer on 4/16/25.
+//
+
+import Testing
+@testable import apple_lab06_05_NavigationWithTabBarController
+
+struct apple_lab06_05_NavigationWithTabBarControllerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
